926ddda912a7e78fd1570c3c10aa2ee383985eac
Onsidelyrics <50561435+Onsidelyrics@users.noreply.github.com>
2020-05-27 15:49:54 +1200

    created Sighn up DTO

---
+++ DTO/signUpForm.js
@@ -0,0 +1,13 @@
+
+class signUpFrom{
+    constructor(username, password, fullname, email, affiliation, gender, age)
+    {
+        this.username = username;
+        this.affiliation = affiliation;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+    }
+}
