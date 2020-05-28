9eda4deb9ed1b45990fa29c52117a22f032bea12
Harrison Cloake <Harrison.Cloake@vista.co>
2020-05-28 19:06:40 +1200

    Finished off the article submit function. Load test done, working correctly.

--- routes/articlesubmit.js
+++ routes/articlesubmit.js
@@ -1,113 +1,137 @@
 var express = require("express");
 var router = express.Router();
 
-const { Client } = require("pg");
+const { Pool } = require("pg");
+let theId;
+let articleStatusId;
+let articleId;
 
 /* Create new pool*/
 let conString =
   process.env.DATABASE_URL ||
-  "postgres://mjlrcdotwouger:9234e9e5235b549e9389d1aab578a2c144306c4ab61b1acd052dfb87ffc645c1@ec2-35-174-127-63.compute-1.amazonaws.com:5432/da9rg6k174929g";
-const client = new Client({
+  "postgres://avrxayow:WsRhIGQ6XcW_MGdgDP47y8udjUnPUpiD@rosie.db.elephantsql.com:5432/avrxayow";
+const pool = new Pool({
   connectionString: conString,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-function connectDatabase() {
-  client.connect(function (err) {
-    if (err) {
-      console.log("Can not connect to the DB" + err);
-    } else {
-      console.log("connected to database");
-    }
+router.post("/submitarticle", async (req, res) => {
+  var data = req.body;
+  var wasError = false;
+
+  await insertReference(data).catch((error) => {
+    console.log(error);
+    res.statusCode = 500;
+    res.send({ ResponseText: error });
+    wasError = true;
+  });
+
+  console.log(
+    "Pool count:" + pool.totalCount + " Waiting count:" + pool.waitingCount
+  );
+
+  console.log("insert done with id " + theId);
+  if (!wasError) {
+    await getArticleStatus("New").catch((error) => {
+      console.log(error);
+      res.statusCode = 500;
+      wasError = true;
+      res.send({ ResponseText: error });
+    });
+  }
+
+  console.log(
+    "Pool count:" + pool.totalCount + " Waiting count:" + pool.waitingCount
+  );
+
+  if (!wasError) {
+    await insertArticle(articleStatusId, theBibId, data.userId).catch(
+      (error) => {
+        console.log(articleStatusId + ":" + theBibId + ":" + data.userId);
+        console.log(error);
+        res.statusCode = 500;
+        wasError = true;
+        res.send({ ResponseText: error });
+      }
+    );
+  }
+
+  console.log(
+    "Pool count:" + pool.totalCount + " Waiting count:" + pool.waitingCount
+  );
+
+  if (!wasError) res.send({ ResponseText: articleId });
+});
+
+async function insertReference(data) {
+  console.log(data);
+  let promise = await new Promise((resolve, reject) => {
+    setTimeout(() => reject("Timeout"), 10000);
+    pool.connect((err, client, release) => {
+      if (err) {
+        reject(err.message);
+      }
+      client
+        .query(
+          `insert into bibliographicreference  (article , author, title, journal, journalYear, journalVolume, journalNumber, pagesFrom, pagesTo, journalMonth ) values ('${data.article}','${data.author}','${data.title}','${data.journal}','${data.year}','${data.volume}','${data.number}','${data.pagefrom}','${data.pageto}','${data.month}' ) RETURNING id`
+        )
+        .then((res) => {
+          theBibId = res.rows[0].id;
+          resolve();
+        })
+        .catch((err) => reject(err.message))
+        .then(() => client.release());
+    });
   });
 }
 
-router.post("/submitarticle", async (req, res) => {
-  var data = req.body;
-  connectDatabase();
-  insertReference(data);
-});
+async function getArticleStatus(statusText) {
+  let promise = await new Promise((resolve, reject) => {
+    setTimeout(() => reject("Timeout"), 10000);
+    pool.connect((err, client, release) => {
+      if (err) {
+        reject(err.message);
+      }
 
-function insertReference(data) {
-  console.log(data);
-  client
-    .query(
-      "insert into bibliographicreference  (article , author, title, journal, journalYear, journalVolume, journalNumber, pagesFrom, pagesTo, journalMonth ) values ('" +
-        data.article +
-        "','" +
-        data.author +
-        "','" +
-        data.title +
-        "','" +
-        data.journal +
-        "','" +
-        data.year +
-        "','" +
-        data.volume +
-        "','" +
-        data.number +
-        "','" +
-        data.pagefrom +
-        "','" +
-        data.pageto +
-        "','" +
-        data.month +
-        "' ) RETURNING id"
-    )
-    .then((res) => {
-      var returnedId = res.rows[0].id;
-      //call insertArticle, need to figure out the user aspect first.
-    })
-    .catch(
-      (err) =>
-        function (err) {
-          console.log(err.stack);
-          //do some stuff to return error to client.
-        }
-    );
+      client
+        .query(
+          "select id from articlestatus where statusDescription = '" +
+            statusText +
+            "'"
+        )
+        .then((res) => {
+          console.log("article status id: " + res.rows[0].id);
+          articleStatusId = res.rows[0].id;
+          resolve();
+        })
+        .catch((err) => reject(err.message))
+        .then(() => client.release());
+    });
+  });
 }
 
-function insertArticle(data) {
-  statusId = getArticleStatus("New");
-  client
-    .query(
-      "insert into Article(status , bibliographicReferenceId, userSubmitted) (" +
-        statusId +
-        ", '" +
-        data.id +
-        "' , " +
-        data.userId +
-        ")"
-    )
-    .then(console.log(res.rows[0].id))
-    .catch(
-      (err) =>
-        function (err) {
-          console.log(err.stack);
-          res.err;
-          //do some stuff to return error to client.
-        }
-    );
-}
-
-function getArticleStatus(statusText) {
-  client
-    .query(
-      "select id from articlestatus where statusDescription = '" +
-        statusText +
-        "'"
-    )
-    .then(console.log(res.rows[0].id))
-    .catch(
-      (err) =>
-        function (err) {
-          console.log(err.stack);
-          res.err;
-          //do some stuff to return error to client.
-        }
-    );
+async function insertArticle(theStatus, theBibId, theUser) {
+  let promise = await new Promise((resolve, reject) => {
+    setTimeout(() => reject("Timeout"), 10000);
+    pool.connect((err, client, release) => {
+      if (err) {
+        reject(err.message);
+      }
+      client
+        .query(
+          `insert into Article(status , bibliographicReferenceId, userSubmitted) values (${theStatus}, ${theBibId}, ${theUser}) RETURNING id`
+        )
+        .then((res) => {
+          console.log("article id: " + res.rows[0].id);
+          articleId = res.rows[0].id;
+          resolve();
+        })
+        .catch((err) => reject(err.message))
+        .then(() => client.release());
+    });
+  });
 }
 
 module.exports = router;
