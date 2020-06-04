7560e4fb334216a5063dbf11e4c3b2c89275c6ca
Harrison Cloake <Harrison.Cloake@vista.co>
2020-06-04 23:11:24 +1200

    Auth working, stores JWT and userID in cookies.

--- config/passport.js
+++ config/passport.js
@@ -1,53 +1,62 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 let conString =
-    process.env.DATABASE_URL ||
-    "postgres://ptoxwxykmkqxkj:c4ce46ef9bdc80563e1d4b90fb65b48a7829e42a07b526c5f8c05cb6fc17bcf2@ec2-35-153-12-59.compute-1.amazonaws.com:5432/ddp53njp2q9370";
+  process.env.DATABASE_URL ||
+  "postgres://ptoxwxykmkqxkj:c4ce46ef9bdc80563e1d4b90fb65b48a7829e42a07b526c5f8c05cb6fc17bcf2@ec2-35-153-12-59.compute-1.amazonaws.com:5432/ddp53njp2q9370";
 const pool = new Pool({
-    connectionString: conString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+  connectionString: conString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 passport.serializeUser(function (user, done) {
-    done(null, user);
+  done(null, user);
 });
 passport.deserializeUser(function (user, done) {
-    done(null, user);
+  done(null, user);
 });
 
-passport.use('local', new LocalStrategy({passReqToCallback: true}, (req, done) => {
-        loginAttempt();
+passport.use(
+  "local",
+  new LocalStrategy({ passReqToCallback: true }, function (
+    req,
+    token,
+    ok,
+    done
+  ) {
+    loginAttempt();
 
-        async function loginAttempt() {
-            const client = await pool.connect()
-            try {
-                await client.query('BEGIN')
-                var currentAccountsData = await JSON.stringify(client.query(`SELECT id, fullname, username, password FROM "User" WHERE "username"='${req.body.username}'`, function (err, result) {
-                    if (err) {
-                        console.log(err)
-                    }
-                    else if (result.rows[0] == null) {
-                        req.flash('danger', "Oops. Incorrect login details.");
-                        return done(null, false);
-                    }
-                    else{
-                        if(result.rows[0].password === req.body.password){//password match
-                            return done(null, false);
-                        }
-                        else{
-                            req.flash('danger', "Oops. Incorrect login details.");
-                            return done(null, false);
-                        }
-                    }
-                }))
-            } catch (e) {
-                throw (e);
+    async function loginAttempt() {
+      const client = await pool.connect();
+      try {
+        await client.query("BEGIN");
+        var currentAccountsData = await JSON.stringify(
+          client.query(
+            `SELECT id, fullname, username, password FROM "User" WHERE "username"='${req.body.username}'`,
+            function (err, result) {
+              if (err) {
+                console.log(err);
+              } else if (result.rows[0] == null) {
+                req.flash("danger", "Oops. Incorrect login details.");
+                return done(null, false);
+              } else {
+                if (result.rows[0].password === req.body.password) {
+                  //password match
+                  return done(null, result.rows[0].username, result.rows[0].id);
+                } else {
+                  req.flash("danger", "Oops. Incorrect login details.");
+                  return done(null, false);
+                }
+              }
             }
-        };
-
+          )
+        );
+      } catch (e) {
+        throw e;
+      }
     }
-))
+  })
+);
