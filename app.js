ea9e0aeb1545aaacba99cbe2e2ff765ffeabf5e3
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2020-06-05 11:37:17 +1200

    Development (#9)
    * Connected to Database
    
    * Added back User.js
    
    Accidentally deleted it
    
    * Added database connection
    
    Search box, returns what was written in it
    
    * Update articlesearch.js
    
    Able to search article name
    Has space issue
    
    * Updated Database Credentials
    
    Fixed search
    
    * Update articlesearch.js
    
    Added similar search  so when article or title contains input
    
    * Added approved filter
    
    Removed testAPI
    Added approved filter
    Cleared up old code
    
    * Adding article submission functionality - Work in progress
    
    * Finished off the article submit function. Load test done, working correctly.
    
    * Finished off the article submit function. Load test done, working correctly.
    
    * New Cleaner Search Article
    
    Redid the whole search
    Now easier to add in constraints(filters)
    Added in Date From and To
    
    * Added Method Constraint
    
    Added method filter
    Fixed Date From and To Bugs
    
    * Update articlesearch.js
    
    Ready for Search for front end
    
    * Update articlesearch.js
    
    fixed clear up each request
    
    * Update articlesearch.js
    
    returns array instead
    
    * Adding support for book submission type
    
    * configred app.js and installed mongo for testing
    
    Testing out a different technology to speed up development, will attempt to switch back to postgres
    
    * Almost there with this BAU
    
    * Update articlesearch.js
    
    Added ascending and descending
    
    * Update articlesearch.js
    
    Fixed asc bug
    
    * Update articlesearch.js
    
    * login session
    
    * Auth working, stores JWT and userID in cookies.
    
    * Adding signup functionality
    
    * Further changes to backend signup/signin
    
    * Delete BE-ENSE701-S1-2020-GROUP3.iml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml
    
    * Delete workspace.xml
    
    Co-authored-by: Tatsuki Hashimoto <xhg3155@autuni.ac.nz>
    Co-authored-by: Harrison Cloake <Harrison.Cloake@vista.co>
    Co-authored-by: Duvvvy <ryanbircham@gmail.com>

--- app.js
+++ app.js
@@ -1,47 +1,91 @@
 var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./testAPI");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const errorHandler = require("errorhandler");
+
+require("./config/passport");
+
+//var indexRouter = require("./routes/index");
 var searchArticleRouter = require("./routes/articlesearch");
-var cors = require("cors");
-var app = express();
+var submitArticleRouter = require("./routes/articlesubmit");
+var auth = require("./routes/auth");
+var join = require("./routes/signup");
+
+//Is this prod or dev?
+const isProduction = process.env.NODE_ENV === "production";
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require("morgan")("dev"));
+
+//TODO CHANGE THE SECRET
+app.use(
+  session({
+    secret: "secret",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//TODO CHANGE THE SECRET
+if (!isProduction) {
+  app.use(errorHandler());
+}
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("views", path.join(__dirname, "views")); //Remove?
+app.set("view engine", "jade"); //Remove?
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(require("morgan")("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser()); //What is this?
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
+//app.use("/", indexRouter);
 app.use("/articlesearch", searchArticleRouter);
+app.use("/article", submitArticleRouter);
+app.use("/auth", auth);
+app.use("/signup", join);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+//Error handlers & middlewares
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+} else {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
+}
 
 //Notes
 console.log("http://localhost:9000");
